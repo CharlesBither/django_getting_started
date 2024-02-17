4f3d0beedcac7b8d44e5891ed3e676fc0249ca75
Charles Bither <95773999+CharlesBither@users.noreply.github.com>
2024-02-16 22:29:29 -0800

    add website and meeting apps

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+
+
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("This is the about page")
